7367fad8efc7dca174a49b1e5527691117e1c67e
FZCONSTRUCOES <fzconstrucoes1995@gmail.com>
2026-05-22 17:01:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
 
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("ativo");
+});
+
+const linksMenu = document.querySelectorAll(".menu a");
+
+linksMenu.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("ativo");
+  });
+});
